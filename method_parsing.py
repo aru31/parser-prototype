bf0c34e2438b8eca09c27e4c56489df21a6d8d76
Arpit Gupta <guptarpit1997@gmail.com>
2019-03-22 19:02:55 +0530

    Complete text parsing

---
+++ method_parsing.py
@@ -0,0 +1,19 @@
+import os
+
+
+def function_parsing(members):
+    """
+    parsing all the public member functions of the header file
+    param: functions is a list of all the methods
+    """
+    name = []
+    return_type = []
+    is_virtual = []
+    is_static = []
+    for member in members:
+        name.append(member[0].split(": ")[1].strip().replace("'", ""))
+        return_type.append(member[6].split(": ")[1].strip())
+        is_virtual.append(member[10].split(": ")[1].strip())
+        is_static.append(member[14].split(": ")[1].strip())
+
+    return name, return_type, is_virtual, is_static
